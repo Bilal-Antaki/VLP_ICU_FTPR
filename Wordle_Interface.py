f0ec138286b7270eef391057931e7235e160a638
Bilal-Antaki <bilal.antaki.1@gmail.com>
2023-08-26 21:28:41 +0300

    Added table borders

--- Wordle_Interface.py
+++ Wordle_Interface.py
@@ -8,28 +8,39 @@ def main ():
     wordle = Wordle("APPLE")
     
     while wordle.can_attempt:
-        x = input ("Enter a guess: ")
+        x = input ("\nEnter a guess: ")
 
         if len(x) != wordle.WORD_LENGTH:
             print(f"Word must be of {wordle.WORD_LENGTH} characters long.")
             continue
-
+        
         x = x.upper()
         wordle.attempt(x)
         display_results(wordle)
 
     if wordle.is_solved:
-        print("You have solved the wordle!")
+        print("\nYou have solved the wordle!\n")
     else:
-        print("You failed ")
+        print("\nYou failed.\n")
 
 
 def display_results (wordle: Wordle):
     
+    lines = []
+
     for word in wordle.attempts:
         result = wordle.guess(word)
         colored_result_str = convert_result_to_color(result)
-        print(colored_result_str)
+        lines.append(colored_result_str)
+    
+    for _ in range (wordle.remaining_attempts):
+        lines.append(" ".join(["_"] * wordle.WORD_LENGTH))
+
+    if not wordle.is_solved:
+        print (f"\nYou have {wordle.remaining_attempts} attempts remaining.")
+    
+    draw_border_around(lines)
+
 
 def convert_result_to_color (result: List[LetterState]):
     result_with_color = []
@@ -44,7 +55,21 @@ def convert_result_to_color (result: List[LetterState]):
         letter_with_color = color + letter.character + Fore.RESET
         result_with_color.append(letter_with_color)
     
-    return "".join(result_with_color)
+    return " ".join(result_with_color)
+
+def draw_border_around (lines: List[str], size: int = 9, pad: int = 1):
+    border_length = size + pad * 2
+    top_border = "┌" + "─" * border_length + "┐"
+    bottom_border = "└" + "─" * border_length + "┘"
+    space = " " * pad
+
+    print(top_border)
+
+    for line in lines:
+        print ("|" + space + line + space + "|")
+
+    print(bottom_border)
+
 
 
 if __name__ == "__main__":
