bc54a698ca6acabada713af145840789ece43be8
Bilal-Antaki <bilal.antaki.1@gmail.com>
2023-08-26 15:46:41 +0300

    project initial stuff

--- Wordle_Code.py
+++ Wordle_Code.py
@@ -1,1 +1,11 @@
-import random as rd
+class Wordle:
+    
+    MAX_ATTEMPTS = 6
+    WORD_LENGTH = 5
+    
+
+    def __init__ (self, secret: str):
+        self.secret: str = secret
+        self.attempts = []
+        
+        pass
--- Wordle_Interface.py
+++ Wordle_Interface.py
@@ -1,1 +1,9 @@
-import random as rd
+from Wordle_Code import Wordle
+
+def main ():
+    wordle = Wordle()
+    print (wordle)
+
+
+if __name__ == "__main__":
+    main()
